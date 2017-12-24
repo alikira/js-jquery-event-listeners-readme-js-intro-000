f0eb0f87b2e8fdd3e58d1b9b79b9ef5f5c5aeb09
Ali Karchoud <karchoud.ali@gmail.com>
2017-12-24 06:24:31 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,18 @@
 //define functions here
 function getIt(){
-  $('p').on('click',function{
+  $('p').on('click',function(){
     alert('Hey!');
   });
 }
+
+function frameIt(){
+  $('img').on('load',function(){
+    
+  })
+}
+
+
+
 $(document).ready(function(){
 
 // call functions here
